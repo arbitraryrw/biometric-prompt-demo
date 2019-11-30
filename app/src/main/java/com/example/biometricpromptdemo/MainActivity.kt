75b0b13558839623d1bd1b9dc5b4a93ea82e7aec
Nikola Cucakovic <work@nik.re>
2019-11-30 23:17:23 +0000

    Adding example to showcase usage of crypto object with biometrics

--- app/src/main/java/com/example/biometricpromptdemo/MainActivity.kt
+++ app/src/main/java/com/example/biometricpromptdemo/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.biometricpromptdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
+
+//Crypto APIs
+import java.security.KeyStore
 import java.util.concurrent.Executor
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 
 //Ref: https://developer.android.com/training/sign-in/biometric-auth#kotlin
 
@@ -17,6 +25,8 @@ class MainActivity : AppCompatActivity() {
     object irrelevantVars{
         var counter = 0
     }
+
+    val KEY_NAME = "dummy"
 
     private lateinit var executor:Executor
     private lateinit var biometricPrompt: BiometricPrompt
@@ -38,6 +48,30 @@ class MainActivity : AppCompatActivity() {
         setupBiometricPromptAndInfo()
 
         biometricManager = BiometricManager.from(this)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            generateSecretKey(KeyGenParameterSpec.Builder(
+                KEY_NAME,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                .setUserAuthenticationRequired(true)
+                // Invalidate the keys if the user has registered a new biometric
+                // credential, such as a new fingerprint. Can call this method only
+                // on Android 7.0 (API level 24) or higher. The variable
+                // "invalidatedByBiometricEnrollment" is true by default.
+                .setInvalidatedByBiometricEnrollment(true)
+                .build())
+        }
+        else{
+            generateSecretKey(KeyGenParameterSpec.Builder(
+                KEY_NAME,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                .setUserAuthenticationRequired(true)
+                .build())
+        }
 
         mainButton.setOnClickListener {
             when (biometricManager.canAuthenticate()) {
@@ -114,6 +148,27 @@ class MainActivity : AppCompatActivity() {
         irrelevantVars.counter++
 
         authResultTextView.setText("Successfully Authenticated:" + irrelevantVars.counter)
+    }
+
+    private fun generateSecretKey(keyGenParameterSpec: KeyGenParameterSpec) {
+        val keyGenerator = KeyGenerator.getInstance(
+            KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+        keyGenerator.init(keyGenParameterSpec)
+        keyGenerator.generateKey()
+    }
+
+    private fun getSecretKey(): SecretKey {
+        val keyStore = KeyStore.getInstance("AndroidKeyStore")
+
+        // Before the keystore can be accessed, it must be loaded.
+        keyStore.load(null)
+        return keyStore.getKey(KEY_NAME, null) as SecretKey
+    }
+
+    private fun getCipher(): Cipher {
+        return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                + KeyProperties.BLOCK_MODE_CBC + "/"
+                + KeyProperties.ENCRYPTION_PADDING_PKCS7)
     }
 
 
